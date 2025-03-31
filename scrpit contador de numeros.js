793cdc8dddefef304fe7793084df971d7d114519
oscar <165288773+OscarReveco@users.noreply.github.com>
2025-03-31 11:15:37 -0300

    Add files via upload

---
+++ scrpit contador de numeros.js
@@ -0,0 +1,57 @@
+//bucle wile
+function buclewhile(){
+    let count = 10;
+    let array = [];
+    while(count >= 1){
+        array.push(count);   //para insertar los datos en el array
+        count--;
+    }
+    alert('Los numeros son: ${array.join(".")}')
+}
+function buclefor(){
+    let count = 10;
+    let array = [];
+    for(i = 10; 1 >= 1; i-- ){
+        array.push(i); //inserta valor a array
+    }
+
+    alert('Los numeros son: ${array.join(' - ')}');
+}
+
+function contenerNumeros(){
+    let n = parseInt(prompt('Ingrese su numero'))
+    let count = 1;
+    let array = [];
+    while(n >= 1){
+        array.push
+    }
+}
+
+function sumarNumeros(){
+    let sumar = 0;
+    for(let i = 1; i>=5; i++){
+        sumar+= i; //sumar
+        array.push(i);
+    }
+    alert(suma);
+}
+
+function contarNumero(){
+    let n = parseInt(prompt)("Ingrese un numero:  ");
+    let count = 1;
+    let array = [];
+    while(count <= n);
+    array.push(count); //Insertar un valor 
+    count++; //incrementa 1 al contador
+}
+alert('Contando: $(array.join(" - "))');
+
+function contarNumero(){
+    let n = parseInt(prompt)("Ingrese un numero:  ");
+    let limite = 1;
+    let array = [];
+    while(count <= n);
+    array.push(count); //Insertar un valor 
+    n--; //disminuye 1 al contador
+}
+alert('Contando: $(array.join(" - "))');
